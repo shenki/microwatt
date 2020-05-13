e53134863d983bc4064e26ed1bcaf6f684e807d6
Joel Stanley <joel@jms.id.au>
2020-05-14 00:38:58 +0930

    loader: Support dtbImage
    This is an image that contains an embedded device tree. A new option is
    presented:
    
    	dtbImage.microwatt:
    	 mw_debug -b jtag stop load dtbImage.microwatt 500000 start
    	 press 'w' to start'
    
    Use the kernel tree at https://github.com/shenki/linux/commits/microwatt-5.7.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- loader/loader.c
+++ loader/loader.c
@@ -5,7 +5,7 @@
 
 #include "console.h"
 
-extern void load_linux(long dtb);
+extern void load_linux(unsigned long kernel, unsigned long dtb);
 extern void invalidate_icache(void);
 
 extern int simple_printf(char *fmt, ...);
@@ -13,27 +13,51 @@ extern int simple_printf(char *fmt, ...);
 
 #define DTB_ADDR	0x01000000L
 
+#define DTBIMAGE_ADDR 	0x500000L
+
 int main(void)
 {
 	int i;
 	potato_uart_init();
+	unsigned long kernel, dtb;
 
 	printf("\r\nMicrowatt Loader (%s %s)\r\n\r\n", __DATE__, __TIME__);
 
 	writeq(SYS_REG_CTRL_DRAM_AT_0, SYSCON_BASE + SYS_REG_CTRL);
 	invalidate_icache();
 
-	printf("Load binaries into SDRAM and press 'l' to start:\r\n\r\n");
-	printf(" mw_debug -b jtag stop load vmlinux.bin load microwatt.dtb %x start\r\n\r\n",
+	printf("Load binaries into SDRAM and select option to start:\r\n\r\n");
+	printf("vmlinux.bin and dtb:\r\n");
+	printf(" mw_debug -b jtag stop load vmlinux.bin load microwatt.dtb %x start\r\n",
 			DTB_ADDR);
+	printf(" press 'l' to start'\r\n\r\n");
+
+	printf("dtbImage.microwatt:\r\n");
+	printf(" mw_debug -b jtag stop load dtbImage.microwatt %x start\r\n",
+			DTBIMAGE_ADDR);
+	printf(" press 'w' to start'\r\n\r\n");
 
 	while (1) {
-		if (getchar() == 'l') {
-			printf("Loading Linux...\r\n");
-			for (i = 0; i < 80; i++)
-				putchar('.');
-			printf("\r\n");
-			load_linux(DTB_ADDR);
+		switch (getchar()) {
+		case 'l':
+			kernel = 0;
+			dtb = DTB_ADDR;
+			goto load;
+		case 'w':
+			kernel = DTBIMAGE_ADDR;
+			dtb = -1;
+			goto load;
+		default:
+			continue;
 		}
 	}
+
+load:
+	printf("Loading Linux at %08x...\r\n", 0x0);
+	for (i = 0; i < 80; i++)
+		putchar('.');
+	printf("\r\n");
+	load_linux(kernel, dtb);
+
+	while (1);
 }
